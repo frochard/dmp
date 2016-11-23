bf98e18a4eb1038d2d47d190b994441d99c761f9
Frederic Rochard <frederic.rochard@gmail.com>
2016-11-23 14:49:53 +0100

    adds some function for mongodb in javascript

--- js/mongodb.js
+++ js/mongodb.js
@@ -9,4 +9,19 @@ MongoClient.connect("mongodb://localhost:27017/dmp", function(err, db) {
     if(!err) {
         console.log("We are connected");
     }
+    //Creation of fingerprints collection
+    var collection = db.collection('fingerprints');
+
+    //Documents storage
+    var docs = [{mykey:1,fingerprint:'fp1'}, {mykey:2,fingerprint:'fp2'}, {mykey:3,fingerprint:'fp3'}];
+    collection.insert(docs, {w:1}, function(err, result) {
+    });
+
+    //retrieve a document from key
+    collection.findOne({mykey: 1}, function (err, item) {
+        console.log(item);
+    });
+
 });
+
+
